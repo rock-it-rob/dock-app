71a3cef5df72fc4b4e0c8e69925ba64df1e9b1ca
Rob Benton <rob.benton@arcourts.gov>
2017-01-04 09:49:24 -0600

    New files

---
+++ dock-app-web/src/main/java/dock/rob/rs/NameEntry.java
@@ -0,0 +1,21 @@
+package dock.rob.rs;
+
+import javax.ws.rs.Path;
+import javax.ws.rs.GET;
+
+
+/**
+ * <code>NameEntry</code> is a JAX-RS class serving rest methods that
+ * provide CRUD operations on a {@ NameRequest} instance.
+ *
+ * @author Rob Benton
+ */
+@Path("/nameentry")
+public class NameEntry
+{
+  @GET
+  public String get()
+  {
+    return "test";
+  }
+}
---
+++ dock-app-web/src/main/java/dock/rob/rs/NameEntryApplication.java
@@ -0,0 +1,14 @@
+package dock.rob.rs;
+
+import javax.ws.rs.ApplicationPath;
+
+import javax.ws.rs.core.Application;
+
+
+/**
+ * <code>NameEntryApplication</code> is a JAX-RS application.
+ *
+ * @author Rob Benton
+ */
+@ApplicationPath("/")
+public class NameEntryApplication extends Application {}
